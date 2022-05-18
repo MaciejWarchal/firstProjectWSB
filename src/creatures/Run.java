c13fc782c71cb23c03a3a1fc579bdb027a58fdd5
Maciej Warchał <warchal.maciej@gmail.com>
2022-05-18 10:07:43 +0200

    selling beta

---
+++ src/creatures/Run.java
@@ -0,0 +1,30 @@
+import creatures.Animal;
+import creatures.*;
+import device.*;
+
+public class Run {
+    public static void main(String[] args){
+
+        Pet kotRex= new Pet("cat","rex",1.0, 1, true);
+        Pet piesReksio= new Pet("reksio", true);
+        Phone Nokia3310= new Phone("3310","Nokia",2000 , 011001010, 25);
+        Car confortableCar=new Car("607","Peugeot", 2002, 240000, 2.2
+                , "black",8000);
+        Animal.Human Grzegorz = new Animal.Human("Grzegorz","Brzenczyszczykiewicz",true,
+                null, kotRex, confortableCar,5000.1,100000.1);
+        Human Stefan= new Animal.Human("Stefan","Spalina", true, null,null,
+                confortableCar,5000 , 25000);
+
+        confortableCar.sell(Grzegorz, Stefan, 8500);
+
+
+
+
+
+
+
+
+
+
+    }
+}
